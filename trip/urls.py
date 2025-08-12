5228b9bb76c2bdfa2483d4ab08df821d132309e7
Olaitan Adedokun <kenzdex@gmail.com>
2025-08-12 12:58:26 +0100

    enable images to be store in the db and migrate models

--- trip/urls.py
+++ trip/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import HomeView
 
 urlpatterns = [
     path('', HomeView.as_view()),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
