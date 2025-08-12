c0a3d67f00d7be1c0a1a61f13690dec533173b96
Olaitan Adedokun <kenzdex@gmail.com>
2025-08-12 13:08:07 +0100

    display the list of the countries and cities

--- trip/urls.py
+++ trip/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import HomeView
+from .views import HomeView, trip_list
 
 urlpatterns = [
-    path('', HomeView.as_view()),
+    path('', HomeView.as_view(), name='home'),
+    path('dashboard/', trip_list, name='trip_list'),
 ]
 
 if settings.DEBUG:
--- trip/views.py
+++ trip/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, HttpResponse
 from django.views.generic import TemplateView
+from .models import Trip
 
 class HomeView(TemplateView):
     template_name = 'trip/index.html'
+
+
+def trip_list(request):
+    trips = Trip.objects.all()
+    context = {
+        'trips': trips
+        }
+    return render(request, 'trip/trip_list.html', context)
